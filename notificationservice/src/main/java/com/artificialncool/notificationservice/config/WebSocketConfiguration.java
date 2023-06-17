61641ae8da8a99f4147236ea320f1f8ccfdbd3d5
Voland24 <luka99al@gmail.com>
2023-06-17 15:27:29 +0200

    feat(user_settings)!: Finished notification handling on the backend

--- notificationservice/src/main/java/com/artificialncool/notificationservice/config/WebSocketConfiguration.java
+++ notificationservice/src/main/java/com/artificialncool/notificationservice/config/WebSocketConfiguration.java
@@ -34,7 +34,8 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
         registry.setApplicationDestinationPrefixes("/socket-subscriber") // Prefiks koji koji se koristi za mapiranje svih poruka.
                 // Klijenti moraju da ga navedu kada salju poruku serveru.
                 // Svaki URL bi pocinjao ovako: http://localhost:8080/socket-subscriber/…/…
-                .enableSimpleBroker("/socket-publisher"); // Definisanje topic-a (ruta) na koje klijenti mogu da se pretplate.
+                .enableSimpleBroker("/socket-publisher/Guest_hostAnsweredReservation","/socket-publisher/Host_createdReservation","/scoket-publisher/Host_cancelledReservation" +
+                                                    "/socket-publisher/Host_stayBeenRated", "/scoket-publisher/Host_hostBeenRated", "/socket-publisher/Host_automaticAcceptance" ); // Definisanje topic-a (ruta) na koje klijenti mogu da se pretplate.
         // SimpleBroker cuva poruke u memoriji i salje ih klijentima na definisane topic-e.
         // Server kada salje poruke, salje ih na rute koje su ovde definisane, a klijenti cekaju na poruke.
         // Vise ruta odvajamo zarezom, npr. enableSimpleBroker("/ruta1", "/ruta2");
